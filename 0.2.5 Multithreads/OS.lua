15dbb0a943685b47d3a9c72516f83e681677bf87
HeroBrine1st <herobrine1.videos@gmail.com>
2018-04-17 20:19:49 +0300

    Update OS.lua

--- 0.2.5 Multithreads/OS.lua
+++ 0.2.5 Multithreads/OS.lua
@@ -18,9 +18,9 @@ table.sort(_G.applications,function(a,b) return a.name < b.name end)
 while true do
 	local nextFile = nextFile or HOME
 	if MT.running == nil then
-		local func = loadfile("/service/zygote.lua",loadfile(nextFile))
+		local func = loadfile("/service/zygote.lua")
 		local i = MT.create(func,nextFile)
-		local success, reason = MT.resume(i,"OPEN_APP")
+		local success, reason = MT.resume(i,loadfile(nextFile),"OPEN_APP")
 		graphics.clearSandbox()
 		if success then nextFile = reason end
 		--if not success then graphics.errorFrame(name,reason) end
