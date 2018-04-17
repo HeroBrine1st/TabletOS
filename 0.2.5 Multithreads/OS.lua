0fef6f710af86e633c84ff98eb1151c62d3277da
HeroBrine1st <herobrine1.videos@gmail.com>
2018-04-17 19:54:48 +0300

    Create OS.lua

---
+++ 0.2.5 Multithreads/OS.lua
@@ -0,0 +1,30 @@
+local core = require("TabletOSCore")
+local graphics = require("TabletOSGraphics")
+local HOME = "/apps/Launcher.lua"
+local apps = "/apps/"
+_G.applications = {}
+local fs = require("filesystem")
+local MT = require("MTCore")
+
+for file in fs.list(apps) do
+	file = fs.concat(apps,file)
+	if file ~= HOME then
+		local success, name = pcall(dofile,file)
+		table.insert(_G.applications,{file=file,name=name}
+	end
+end
+table.sort(_G.applications,function(a,b) return a.name < b.name end)
+
+while true do
+	local nextFile = nextFile or HOME
+	if MT.running == nil then
+		local func = function()
+			loadfile("/service/zygote.lua",loadfile(nextFile))
+		end
+		local i = MT.create(,nextFile)
+		local success, reason = MT.resume(i,"OPEN_APP")
+		graphics.clearSandbox()
+		if success then nextFile = reason end
+		--if not success then graphics.errorFrame(name,reason) end
+	end
+end
