b1ec6f30a4c9b3782c9cffd8f0663b1cc40252cd
HeroBrine1st <herobrine1.videos@gmail.com>
2018-10-05 12:54:29 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 local fs = require("filesystem")
 local args = ({...})
-local strTW = "return \"" .. args[2] .. "\""
+local strTW = "return " .. tostring(args[2]) .. ""
 local f = io.open("/TabletOS/.version","w")
 f:write(strTW)
 f:close()
