fea82a11c4badfc8eb40f0cc2f40b550bdb80d74
HeroBrine1st <herobrine1.videos@gmail.com>
2019-03-02 18:18:37 +0300

    Create installerScript.lua

---
+++ installerScript.lua
@@ -0,0 +1,18 @@
+local args = ({...})[1]
+local channel = args.channel
+local filelist = args.filelist
+local build = filelist.build
+
+local metadata = {}
+metadata.channel = channel.index
+metadata.build = build
+metadata.filelist = {}
+for key, value in pairs(filelist) do
+	if key ~= "build" then
+		table.insert(metadata.filelist,key)
+	end
+end
+local path = "/TabletOS/.vMetadata"
+local f = io.open(path,"w")
+f:write(require("serialization").serialize(metadata))
+f:close()
